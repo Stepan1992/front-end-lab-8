bcf48e99e0e6f38db936cc4681fbac0d8562ee7d
Stepan1992 <chopkostepan1992@gmail.com>
2018-04-17 21:29:53 +0300

    add homework_10

---
+++ homework_10/app.js
@@ -0,0 +1,112 @@
+class Input {
+    constructor(placeHolder) {
+        this.placeHolder = placeHolder || "Type...";
+        this._value = "";
+    };
+
+    get value() {
+        return this._value;
+    };
+
+    setValue(newValue) {
+        this._value = newValue;
+    };
+};
+
+class NumberInput extends Input {
+    constructor(placeHolder) {
+        super(placeHolder);
+        this.type = "number";
+    };
+};
+
+class InputDecorator extends Input {
+    constructor(input) {
+        super();
+        for (let prop in input) {
+            this[prop] = input[prop];
+        };
+    };
+
+    validating(flag, massage) {
+        if (this.flag === false) {
+            if (massage) {
+                this.massage = `${this.massage}, ${massage}`;
+            };
+            return;
+        };
+        this.flag = flag;
+
+        if(this.flag === true){
+            delete this.massage;
+        };
+
+
+        if (massage) {
+            this.massage = massage;
+        };
+    };
+
+    get valid() {
+        if (this.massage && !this.flag) {
+            return `${this.flag} ${this.massage}`;
+        };
+        return `${this.flag} all validators pass`;
+    };
+
+    setValue(newValue) {
+        super.setValue(newValue);
+        delete this.flag;
+    };
+};
+
+class AddNumberValidation extends InputDecorator {
+    constructor(input) {
+        super(input);
+        let type = input.type || 'number';
+        if (typeof input._value === type) {
+            super.validating(true);
+        } else {
+            super.validating(false, 'because of number validator');
+        };
+    };
+};
+
+class AddRequiredValidation extends InputDecorator {
+    constructor(input) {
+        super(input);
+        if (input._value !== '' && typeof input._value !== 'undefined' && input._value !== null) {
+            super.validating(true);
+        } else {
+            super.validating(false, 'because of required validator');
+        };
+    };
+};
+
+class AddMaxLengthValidation extends InputDecorator {
+    constructor(input, maxLength) {
+        super(input);
+        if (String(input._value).length > 0 && String(input._value).length <= maxLength) {
+            super.validating(true);
+        } else {
+            super.validating(false, 'because of max length validator');
+        };
+    };
+};
+
+let numberInput = new NumberInput("Type numbers...");
+
+numberInput = new AddRequiredValidation(numberInput);
+console.log(numberInput.valid);
+
+numberInput.setValue('1');
+numberInput = new AddNumberValidation(numberInput);
+console.log(numberInput.valid);
+
+numberInput.setValue(1);
+numberInput = new AddNumberValidation(numberInput);
+console.log(numberInput.valid);
+
+numberInput.setValue(1111111111111111111111111111);
+numberInput = new AddMaxLengthValidation(numberInput, 5);
+console.log(numberInput.valid);
